022b8ef525fe6da902d44b0cf1e87fb3a44e4e7c
Batuhan <han_batu_1996@hotmail.com>
2024-03-21 15:47:17 +0300

    edits

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,34 @@
 import "../styles/header.scss";
 import { useNavigate } from "react-router-dom";
 import LogoIcon from "../assets/logo.png";
+import React, { useState, useEffect } from "react";
 import Button  from  "../components/button";
 
 //rafce use to function class
 const header = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const navigate = useNavigate();
+  const [showButton, setShowButton] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setShowButton(false);
+      } else {
+        setShowButton(true);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  
   return (
     <div className="navbar background " style={{ zIndex: 1 }}>
+      
       <div className="logo">
-        <img src={LogoIcon} />
+        {showButton  && <img src={LogoIcon} />}
       </div>
       <nav className="list">
         <ul className="nav-list">
